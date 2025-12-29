21bdab7067c0836635907611011484fe33217cc6
Setsuna2207 <setsuna2207.yae@gmail.com>
2025-12-29 22:28:28 +0700

    update create quiz result dto

--- Models/DTOs/QuizResult/CreateQuizResultRequestDTO.cs
+++ Models/DTOs/QuizResult/CreateQuizResultRequestDTO.cs
@@ -3,7 +3,7 @@
     public class CreateQuizResultRequestDTO
     {
         public int QuizID { get; set; }
-        public string UserID { get; set; } = null!;
+        public string? UserID { get; set; } // Make it nullable since controller sets it
         public string? UserAnswers { get; set; }
         public float? Score { get; set; }
         public int? TotalQuestions { get; set; }
